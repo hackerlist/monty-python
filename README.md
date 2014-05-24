17b2afaa1d8c45866f5a3c9ec724c32502979886
Michael E. Karpeles <michael.karpeles@gmail.com>
2014-05-24 03:37:15 -0700

    v.0.0.01 -- initial release (Lancelot)

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ monty-python
 ============
 
 Python API Bindings for the Monty server monitoring Go library
+
+## Installation
+
+For instructions on installing Monty, please refer to the Monty setup
+guide at https://github.com/hackerlist/monty. To install the Python
+API bindings via pypi run the command:
+
+    sudo pip install monty-python
+
+## Usage
+
+    >>> from montypy import Monty
+    >>> m = Monty('http://monty.criticalpha.se', token='XXX')
+    >>> print m.nodes()
---
+++ setup.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*-
+
+"""
+    montypy
+    ~~~~~~~
+
+    Setup
+    `````
+    $ sudo pip install monty-python
+"""
+
+from distutils.core import setup
+import os
+
+setup(
+    name='monty-python',
+    version='0.0.01',
+    url='https://github.com/hackerlist/monty-python',
+    author='hackerlist',
+    author_email='m@hackerlist.net',
+    packages=[
+        'montypy',
+        ],
+    platforms='any',
+    license='LICENSE',
+    install_requires=[
+        'requests >= 1.1.0'
+    ],
+    description="Python API Bindings for the Monty server monitoring Go library",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+)
